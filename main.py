355be158c2d68f195ec96224029a2931f3ce9ff6
filip k <filip2006kujawa@gmail.com>
2023-12-12 14:40:37 +0100

    aaaaaa

--- main.py
+++ main.py
@@ -1,11 +1,17 @@
-import hashlib
-import sys
+# import hashlib
+# import sys
+#
+# if len(sys.argv) == 4:
+#     format = sys.argv[1]
+#     worldlista_sciezka = sys.argv[2]
+#     text_do_odhashowania = sys.argv[3]
+#     typhasha = sys.argv[4]
+#     return format
+#
+# def sprawdztyphasha(text_do_odhashowania)
+# def lamanie_hasla(format,text_do_odhashowania,worldlista_sciezka,typhasha):
+#
+text_do_odhashowania = input("podaj text do odhashowania")
+wordlist = []
 
-if len(sys.argv) == 4:
-    format = sys.argv[1]
-    worldlista_sciezka = sys.argv[2]
-    text_do_odhashowania = sys.argv[3]
-    typhasha = sys.argv[4]
-
-def lamanie_hasla(format,text_do_odhashowania,worldlista_sciezka,typhasha):
-    
+def
