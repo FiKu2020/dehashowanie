27a05a604436346f6f4fda166f1aa926a3734aaf
filip k <filip2006kujawa@gmail.com>
2023-12-18 21:30:33 +0100

    cccc

--- main.py
+++ main.py
@@ -19,24 +19,29 @@ zahaszowany_text = input("podaj text do odhashowania")
 wordlist = []
 
 def breaking_hash(zahaszowany_text, typ_hasha, wordlist):
-   if typ_hasha == 'md5':
-            hash_function = hashlib.md5
-        elif typ_hasha == 'sha256':
-            hash_function = hashlib.sha256
-        elif typ_hasha == 'sha3-512':
-            hash_function = hashlib.sha3_512
-        else:
-            print("zły hash")
-            return
+    if typ_hasha == 'md5':
+        funkcjaHashujaca = hashlib.md5
+    elif typ_hasha == 'sha256':
+        funkcjaHashujaca = hashlib.sha256
+    elif typ_hasha == 'sha3-512':
+        funkcjaHashujaca = hashlib.sha3_512
+    else:
+        print("zły hash")
+        return
    
-            with open(wordlist, "r") as read:
+    with open(wordlist, "r") as read:
+        dane_z_pliku = read.readlines()
+        for line in dane_z_pliku:
+            pass
+                
+            
                    
 
-    if __name__ == "__main__":
-        if len(sys.argv) != 4:
-            print("ok")
-        else:
-            zahaszowany_text = sys.argv[1]
-            typ_hasha = sys.argv[2]
-            Wordlist = sys.argv[3]
-            breaking_hash(zahaszowany_text, typ_hasha, Wordlist)
+if __name__ == "__main__":
+    if len(sys.argv) != 4:
+        print("ok")
+    else:
+        zahaszowany_text = sys.argv[1]
+        typ_hasha = sys.argv[2]
+        Wordlist = sys.argv[3]
+        breaking_hash(zahaszowany_text, typ_hasha, Wordlist)
